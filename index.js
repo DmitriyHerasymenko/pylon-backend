d34930d28d89477b84f87861011b1e8c70530376
Dmytro Herasymenko <dmytroherasymenko@Dmytros-MacBook-Pro.local>
2021-08-26 15:56:41 +0300

    create server, install npm express & pg & nodemon, add get request users

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.get('/users', (req, res) => res.send('hello user'))
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`) )
