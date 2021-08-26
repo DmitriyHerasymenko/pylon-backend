36b635c443f6542ec9a0aef148b58c85a3a9ea92
Dmytro Herasymenko <dmytroherasymenko@Dmytros-MacBook-Pro.local>
2021-08-26 18:57:45 +0300

    add get request users

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -4,11 +4,11 @@ class UserController {
     async createUser (req, res) {
         const {name, mail} = req.body
         const newUser = await db.query(`INSERT INTO users(name, mail)  values($1, $2) RETURNING *`, [name, mail]);
-
         res.json(newUser.rows[0])
     }
     async getUsers (req, res) {
-
+        const newUser = await db.query(`SELECT * FROM users`);
+        res.json(newUser.rows)
     }
     async getUser (req, res) {
 
