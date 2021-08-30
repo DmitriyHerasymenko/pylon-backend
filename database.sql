9b8b5169ca0c9d6a38bc861743ee5d58c9df3763
DmitriyHerasymenko <dmitriy.herasymenko@482.solutions>
2021-08-30 12:28:29 +0300

    refactoring database

--- database.sql
+++ database.sql
@@ -1,19 +1,14 @@
-create TABLE person (
+create TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     mail VARCHAR(255),
     password VARCHAR(255)
 );
 
-create TABLE users (
+create TABLE services (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
-    mail VARCHAR(255)
-);
-
-create TABLE devices (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
+    apikey VARCHAR(255),
     user_id INTEGER,
     FOREIGN KEY(user_id) REFERENCES users (id)
 );
